6bdc3c8079e9bd691351ea95bbf65c963128f65f
Nicholas Dille <nicholas.dille@mailbox.org>
2019-08-19 20:38:07 +0200

    Added user

--- shellinabox/entrypoint.sh
+++ shellinabox/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/dumb-init /bin/bash
+set -e
 
-shellinaboxd --disable-ssl &
+addgroup ${SHELL_GROUP}
+adduser -D -h /home/${SHELL_USER} -s /bin/bash -G ${SHELL_GROUP} ${SHELL_USER}
+
+shellinaboxd --disable-ssl --css=/white-on-black.css -s '/:${SHELL_USER}:${SHELL_GROUP}:/:/bin/bash' &
 
 /usr/local/bin/dind dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375
